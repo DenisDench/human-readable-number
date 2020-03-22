9273436013ce0fb40e631f5e7d2a457da13f1c9d
Denis Salikhov <salihov.den@mail.ru>
2020-03-22 09:08:03 +0700

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
+const digits = {
+    0: '',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+}
+const tens = {
+    0: '',
+    1: '',
+    2: 'twenty',
+    3: 'thirty',
+    4: 'forty',
+    5: 'fifty',
+    6: 'sixty',
+    7: 'seventy',
+    8: 'eighty',
+    9: 'ninety',
+}
+
 module.exports = function toReadable (number) {
+  let result = '';
+  let twoLastDigits = Number(String(number).substr(-2, 2));
+  let lessThen100 = '';
   
+  if (twoLastDigits < 20) {
+    lessThen100 = digits[twoLastDigits];
+  };
+  
+  if (20 <= twoLastDigits && twoLastDigits < 100) {
+    for (let key in tens) {
+      if (key == Number(String(twoLastDigits)[0])) {
+        lessThen100 += tens[key];
+      }
+    }
+    for (let key in digits) {
+      if (key == Number(String(twoLastDigits)[1])) {
+        lessThen100 += ' ' + digits[key];
+      }
+    }
+  }
+  if (number == 0) return 'zero';
+  if (number < 100) return lessThen100.trim();
+  if (number >= 100) {
+    for (let key in digits) {
+      if (key == Number(String(number)[0])) {
+        result += digits[key] + ' hundred ';
+      }
+    }
+    return (result + lessThen100).trim();
+  }
 }
